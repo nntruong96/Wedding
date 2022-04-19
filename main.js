14dc0744012588db619e4d6d7d38881898258358
5708646fe8691f1cf581b133aa0b806ac8de5396 <5708646fe8691f1cf581b133aa0b806ac8de5396>
2022-04-19 10:26:57 +0700

    update img

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var countdown = document.querySelector(".countdown");
+var countdown = document.querySelector('.countdown');
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
   var seconds = Math.floor((t / 1000) % 60);
@@ -16,39 +16,39 @@ function getTimeRemaining(endtime) {
 
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector(".days");
-  var hoursSpan = clock.querySelector(".hours");
-  var minutesSpan = clock.querySelector(".minutes");
-  var secondsSpan = clock.querySelector(".seconds");
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
   var newChild;
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
-    var daysArr = String(t.days).split("");
-    daysSpan.innerHTML = "";
+    var daysArr = String(t.days).split('');
+    daysSpan.innerHTML = '';
     for (var i = 0; i < daysArr.length; i++) {
-      newChild = document.createElement("span");
+      newChild = document.createElement('span');
       newChild.innerHTML = daysArr[i];
       daysSpan.appendChild(newChild);
     }
-    var hoursArr = String(("0" + t.hours).slice(-2)).split("");
-    hoursSpan.innerHTML = "";
+    var hoursArr = String(('0' + t.hours).slice(-2)).split('');
+    hoursSpan.innerHTML = '';
     for (var i = 0; i < hoursArr.length; i++) {
-      newChild = document.createElement("span");
+      newChild = document.createElement('span');
       newChild.innerHTML = hoursArr[i];
       hoursSpan.appendChild(newChild);
     }
-    var minuteArr = String(("0" + t.minutes).slice(-2)).split("");
-    minutesSpan.innerHTML = "";
+    var minuteArr = String(('0' + t.minutes).slice(-2)).split('');
+    minutesSpan.innerHTML = '';
     for (var i = 0; i < minuteArr.length; i++) {
-      newChild = document.createElement("span");
+      newChild = document.createElement('span');
       newChild.innerHTML = minuteArr[i];
       minutesSpan.appendChild(newChild);
     }
-    var secondArr = String(("0" + t.seconds).slice(-2)).split("");
-    secondsSpan.innerHTML = "";
+    var secondArr = String(('0' + t.seconds).slice(-2)).split('');
+    secondsSpan.innerHTML = '';
     for (var i = 0; i < secondArr.length; i++) {
-      newChild = document.createElement("span");
+      newChild = document.createElement('span');
       newChild.innerHTML = secondArr[i];
       secondsSpan.appendChild(newChild);
     }
@@ -60,16 +60,16 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 // set your wedding date here
-var deadline = "May 15 2022 11:00:00 GMT+0300";
+var deadline = 'May 15 2022 11:00:00 GMT+0300';
 if (countdown) {
-  initializeClock("timer", deadline);
+  initializeClock('timer', deadline);
 }
 
 /*-- cat run  */
 /*-- show album  */
 let showAlbum = () => {
   jQuery(document).ready(function () {
-    jQuery("#nanogallery2").nanogallery2({
+    jQuery('#nanogallery2').nanogallery2({
       // ### gallery settings ###
       galleryMosaic: [
         // default layout
@@ -108,13 +108,13 @@ let showAlbum = () => {
         { w: 1, h: 1, c: 3, r: 4 },
       ],
       galleryMaxRows: 1,
-      galleryDisplayMode: "rows",
-      gallerySorting: "random",
-      thumbnailDisplayOrder: "random",
+      galleryDisplayMode: 'rows',
+      gallerySorting: 'random',
+      thumbnailDisplayOrder: 'random',
 
-      thumbnailHeight: "180",
-      thumbnailWidth: "220",
-      thumbnailAlignment: "scaled",
+      thumbnailHeight: '180',
+      thumbnailWidth: '220',
+      thumbnailAlignment: 'scaled',
       thumbnailGutterWidth: 0,
       thumbnailGutterHeight: 0,
       thumbnailBorderHorizontal: 0,
@@ -128,54 +128,60 @@ let showAlbum = () => {
       // for gallery
       galleryDisplayTransitionDuration: 1500,
       // for thumbnails
-      thumbnailDisplayTransition: "imageSlideUp",
+      thumbnailDisplayTransition: 'imageSlideUp',
       thumbnailDisplayTransitionDuration: 1200,
-      thumbnailDisplayTransitionEasing: "easeInOutQuint",
+      thumbnailDisplayTransitionEasing: 'easeInOutQuint',
       thumbnailDisplayInterval: 60,
 
       // THUMBNAIL HOVER ANIMATION
-      thumbnailBuildInit2: "image_scale_1.15",
+      thumbnailBuildInit2: 'image_scale_1.15',
       thumbnailHoverEffect2:
-        "thumbnail_scale_1.00_1.05_300|image_scale_1.15_1.00",
+        'thumbnail_scale_1.00_1.05_300|image_scale_1.15_1.00',
       touchAnimation: true,
       touchAutoOpenDelay: 500,
 
       // LIGHTBOX
       viewerToolbar: { display: false },
       viewerTools: {
-        topLeft: "label",
-        topRight:
-          "shareButton, rotateLeft, rotateRight, fullscreenButton, closeButton",
+        topLeft: 'label',
+        topRight: 'rotateLeft, rotateRight, fullscreenButton, closeButton',
       },
 
       // GALLERY THEME
       galleryTheme: {
-        thumbnail: { background: "#111" },
+        thumbnail: { background: '#111' },
       },
 
       // DEEP LINKING
       locationHash: true,
       items: [
-        { src: "./img/a20.jpg", srct: "./img/a20.jpg", },
-        { src: "./img/a12.jpeg", srct: "./img/a12.jpeg", },
-        { src: "./img/a2.jpeg", srct: "./img/a2.jpeg",  },
-        { src: "./img/a3.jpeg", srct: "./img/a3.jpeg", },
-        { src: "./img/a4.jpeg", srct: "./img/a4.jpeg",  },
-        { src: "./img/a5.jpeg", srct: "./img/a5.jpeg",  },
-        { src: "./img/a6.jpeg", srct: "./img/a6.jpeg",  },
-        { src: "./img/a7.jpeg", srct: "./img/a7.jpeg", },
-        { src: "./img/a8.jpeg", srct: "./img/a8.jpeg",  },
-        { src: "./img/a9.jpeg", srct: "./img/a9.jpeg", },
-        { src: "./img/a10.jpeg", srct: "./img/a10.jpeg",  },
-        { src: "./img/a11.jpeg", srct: "./img/a11.jpeg", },
-        { src: "./img/a1.jpeg", srct: "./img/a1.jpeg", },
-        { src: "./img/a17.jpg", srct: "./img/a17.jpg", },
-        { src: "./img/a18.jpg", srct: "./img/a18.jpg", },
-        { src: "./img/a19.jpg", srct: "./img/a19.jpg", },
-        { src: "./img/a20.jpg", srct: "./img/a20.jpg", },
-        { src: "./img/a21.jpg", srct: "./img/a21.jpg", },
+        { src: './img/a20.jpg', srct: './img/a20.jpg' },
+        { src: './img/a12.jpeg', srct: './img/a12.jpeg' },
+        { src: './img/a2.jpeg', srct: './img/a2.jpeg' },
+        { src: './img/a3.jpeg', srct: './img/a3.jpeg' },
+        { src: './img/a4.jpeg', srct: './img/a4.jpeg' },
+        { src: './img/a5.jpeg', srct: './img/a5.jpeg' },
+        { src: './img/a6.jpeg', srct: './img/a6.jpeg' },
+        { src: './img/a7.jpeg', srct: './img/a7.jpeg' },
+        { src: './img/a8.jpeg', srct: './img/a8.jpeg' },
+        { src: './img/a9.jpeg', srct: './img/a9.jpeg' },
+        { src: './img/a10.jpeg', srct: './img/a10.jpeg' },
+        { src: './img/a11.jpeg', srct: './img/a11.jpeg' },
+        { src: './img/a1.jpeg', srct: './img/a1.jpeg' },
+        { src: './img/a17.jpg', srct: './img/a17.jpg' },
+        { src: './img/a18.jpg', srct: './img/a18.jpg' },
+        { src: './img/a19.jpg', srct: './img/a19.jpg' },
+        { src: './img/a20.jpg', srct: './img/a20.jpg' },
+        { src: './img/a21.jpg', srct: './img/a21.jpg' },
+        { src: './img/a22.jpeg', srct: './img/a22.jpeg' },
+        { src: './img/a23.jpeg', srct: './img/a23.jpeg' },
+        { src: './img/a24.jpeg', srct: './img/a24.jpeg' },
+        { src: './img/a25.jpeg', srct: './img/a25.jpeg' },
+        { src: './img/a26.jpeg', srct: './img/a26.jpeg' },
+        { src: './img/a27.jpeg', srct: './img/a27.jpeg' },
+        { src: './img/a28.jpeg', srct: './img/a28.jpeg' },
       ],
     });
   });
 };
-showAlbum()
+showAlbum();
